64a1fa8476a26a86569bf8c4c15425202e21c029
dab <dab@ct.de>
2020-07-19 20:29:28 +0000

    Add files via upload

---
+++ bench.py
@@ -0,0 +1,10 @@
+import cv2
+
+img1 = cv2.imread('messi.jpg')
+e1 = cv2.getTickCount()
+for i in range(5,49,2):
+    img1 = cv2.medianBlur(img1,i)
+e2 = cv2.getTickCount()
+t = (e2 - e1)/cv2.getTickFrequency()
+print( t )
+# Result I got is 0.521107655 seconds
